8451c1833484c2aff292d85043e13d504bb3e9cc
Stephen P. Lepisto <stephen@stephenlepisto.com>
2023-02-25 02:25:54 +0000

    Add argumentnull_error.h, defining a custom C++ exception for null arguments.

---
+++ cplusplus/helpers/argumentnull_error.h
@@ -0,0 +1,53 @@
+#pragma once
+#ifndef __ARGUMENTNULL_ERROR_H__
+#define __ARGUMENTNULL_ERROR_H__
+
+#include <stdexcept>
+#include <string>
+
+namespace Helpers
+{
+    /// <summary>
+    /// Exception for arguments that are null.
+    /// </summary>
+    class argumentnull_error : public std::runtime_error
+    {
+    private:
+        std::string _parameter;
+    public:
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="parm">The parameter that was null.</param>
+        /// <param name="what_arg">Specific message indicating the problem.</param>
+        argumentnull_error(const std::string& parm, const std::string& what_arg)
+            : _parameter(parm)
+            , runtime_error(what_arg)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="parm">The parameter that was null.</param>
+        /// <param name="what_arg">Specific message indicating the problem.</param>
+        argumentnull_error(const char* parm, const char* what_arg)
+            : _parameter(parm != nullptr ? parm : "")
+            , runtime_error(what_arg)
+        {
+        }
+
+        /// <summary>
+        /// Retrieve the parameter associated with the exception.
+        /// </summary>
+        /// <returns>Returns a string containing the name of the parameter.</returns>
+        std::string parameter()
+        {
+            return _parameter;
+        }
+    };
+
+} // end namespace
+
+#endif // __ARGUMENTNULL_ERROR_H__
+
