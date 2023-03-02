29185dd89be7e11029cff9575aeea84b89131157
Stephen P. Lepisto <stephen@stephenlepisto.com>
2023-03-02 06:44:23 +0000

    Rewrite explanation of Visitor_Exercise() method in C# so it fits the new visitor/data model.

--- csharp/Program.cs
+++ csharp/Program.cs
@@ -665,12 +665,12 @@ namespace DesignPatternExamples_csharp
         /// Example of using the Visitor design pattern.
         /// 
         /// The Visitor pattern is used to add functionality to a list of
-        /// otherwise unchanging element objects by passing a function to each
-        /// element object.  Each element object calls the function, passing
-        /// itself to the function.  The visiting function then does something
-        /// based on the type of the element.
+        /// otherwise unchanging element objects by passing a visitor object to
+        /// each element object.  Each element object calls the visitor object,
+        /// passing itself as an argument.  The visitor object then does
+        /// something based on the type of the element.
         /// 
-        /// In this exercise, a collection of shop object is initialized then
+        /// In this exercise, a collection of shop objects is initialized then
         /// an order visitor is created to retrieve an item from one of the shop
         /// objects.  Along the way, shops that don't have the necessary
         /// ingredients use another order visitor to order ingredients from
