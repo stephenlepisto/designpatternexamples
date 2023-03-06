72d3b44a4986ebecc21c0d875e56357645adc738
Stephen P. Lepisto <stephen@stephenlepisto.com>
2023-03-06 08:25:08 +0000

    Add argumentinvalid_error exception helper definition in C++.

---
+++ cplusplus/helpers/argumentinvalid_error.h
@@ -0,0 +1,59 @@
+/// @file
+/// @brief
+/// Implementation of the @ref Helpers::argumentinvalid_error "argumentinvalid_error"
+/// exception.
+
+#pragma once
+#ifndef __ARGUMENT_ERROR_H__
+#define __ARGUMENT_ERROR_H__
+
+#include <stdexcept>
+#include <string>
+
+namespace Helpers
+{
+
+    /// <summary>
+    /// Exception for arguments that are invalid.
+    /// </summary>
+    class argumentinvalid_error : public std::runtime_error
+    {
+    private:
+        std::string _parameter;
+    public:
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="parm">The parameter that was invalid.</param>
+        /// <param name="what_arg">Specific message indicating the problem.</param>
+        argumentinvalid_error(const std::string& parm, const std::string& what_arg)
+            : _parameter(parm)
+            , runtime_error(what_arg)
+        {
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="parm">The parameter that was invalid.</param>
+        /// <param name="what_arg">Specific message indicating the problem.</param>
+        argumentinvalid_error(const char* parm, const char* what_arg)
+            : _parameter(parm != nullptr ? parm : "")
+            , runtime_error(what_arg)
+        {
+        }
+
+        /// <summary>
+        /// Retrieve the parameter associated with the exception.
+        /// </summary>
+        /// <returns>Returns a string containing the name of the parameter.</returns>
+        std::string parameter()
+        {
+            return _parameter;
+        }
+    };
+
+} // end namespace
+
+#endif // __ARGUMENT_ERROR_H__
+
