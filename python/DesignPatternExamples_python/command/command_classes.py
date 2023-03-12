7f20b5d9545d04787f2a2e505ff0810e4740fa45
Stephen P. Lepisto <stephen@stephenlepisto.com>
2023-03-12 06:59:20 +0000

    Translate the Command pattern from C++ to Python.

---
+++ python/DesignPatternExamples_python/command/command_classes.py
@@ -0,0 +1,128 @@
+## @file
+# @brief
+# Implementation of the
+# @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject"
+# and @ref DesignPatternExamples_python.command.command_classes.Command "Command"
+# classes used in the @ref command_pattern.
+
+## Container for a string.  Need to use a class that allows the text to
+#  be changed while the container (this class) remains constant.  This
+#  way, operations can be applied to the text and the container's contents
+#  change but not the container (strings are immutable in Python this dances
+#  around that problem).
+class Command_TextObject:
+
+    ## Constructs a text object with an initial string.
+    #
+    #  @param text
+    #         The initial string for the text object.
+    def __init__(self, text : str):
+        self._text = text
+        self._startingText = text
+
+    ## @var _startingText
+    #       Starting string text so we can reset the text to a known point.
+    ## @var _text
+    #       The text that can change.
+
+
+    ## Gets the text in this TextObject.
+    def Text(self) -> str:
+        return self._text
+
+    ## Sets the text in this TextObject.
+    def SetText(self, value : str):
+        self._text = value
+
+    ## Resets the TextObject to the starting string.
+    def Reset(self):
+        self._text = self._startingText
+
+    ## Returns the TextObject to a string.
+    #
+    #  @returns
+    #     Returns the text stored in the text object.
+    def ToString(self) -> str:
+        return self._text
+
+
+
+## Represents an operation that can be applied to a TextObject.
+#  This class can handle two kinds of operations, one that takes no
+#  additional parameters and one that takes two additional string
+#  parameters.
+# 
+#  In a real program, the commands would be represented by an interface
+#  and concrete classes for each type of operation (based on additional
+#  parameters) would be used.  This requires the calling entity to
+#  instantiate the appropriate concrete class as opposed to letting the Python
+#  constructor figure out the correct operation based on the number of extra
+#  parameters provided for the operation.  Or the calling entity could use a
+#  class factory to create the concrete classes.
+#  
+#  For Python, this Command class is made somewhat more general case simply
+#  because there can only be one `__init__()` method (constructor).  So the
+#  number of extra parameters passed after the function determine whether the
+#  function takes zero or two parameters.  The code assumes the two-parameter
+#  function if there are exactly two extra arguments given to the constructor;
+#  otherwise, a no-parameter function is assumed.
+class Command:
+
+    ## Constructor for a command that applies an operation to a TextObject, where the
+    #  operation takes zero or two parameters.
+    #
+    #  @param source
+    #         The TextObject to apply the operation to.
+    #  @param commandName
+    #         Easy-to-read name of the command.
+    #  @param operation
+    #         The two parameter operation to apply to the TextObject.
+    #  @param arguments
+    #         Zero or two arguments that are passed to the operation
+    def __init__(self, source : Command_TextObject, commandName : str, operation, *arguments) -> None:
+        self._receiver = source
+        self._commandName = commandName
+        self._argument1 = None
+        self._argument2 = None
+        self._two_parameter_operation = None
+        self._no_parameter_operation = None
+        if len(arguments) == 2:
+            self._argument1 = arguments[0]
+            self._argument2 = arguments[1]
+            self._two_parameter_operation = operation
+        else:
+            self._no_parameter_operation = operation
+
+
+    ## @var _receiver
+    #       The receiver of the command.
+    ## @var _commandName
+    #       Easy-to-read command name.
+    ## @var _two_parameter_operation
+    #       Two parameter operation to apply to the receiver.
+    ## @var _no_parameter_operation
+    #       No parameter operation to apply to the receiver.
+    ## @var _argument1
+    #       The first argument to the two-parameter operation.
+    ## @var _argument2
+    #       The second argument to the two-parameter operation.
+
+
+    ## Execute the command on the TextObject.
+    def Execute(self):
+        if self._two_parameter_operation:
+            self._two_parameter_operation(self._receiver, self._argument1, self._argument2)
+        else:
+            self._no_parameter_operation(self._receiver)
+
+    ## Return this command as a string representation.
+    #
+    #  @returns
+    #     A string containing the representation of the command.
+    def ToString(self) -> str:
+        output = "<NO COMMAND>"
+        if self._two_parameter_operation:
+            output = "{0} \"{1}\" with \"{2}\"".format(self._commandName, self._argument1, self._argument2)
+        elif self._no_parameter_operation:
+            output = self._commandName
+        return output
--- python/DesignPatternExamples_python/command/command_exercise.py
+++ python/DesignPatternExamples_python/command/command_exercise.py
@@ -1,24 +1,150 @@
 ## @file
 # @brief
-# Implementation of the Command_Exercise() function as used in the 
-# @ref command_pattern "Command pattern".
+# Implementation of the @ref DesignPatternExamples_python.command.command_exercise.Command_Exercise "Command_Exercise"()
+# function as used in the @ref command_pattern.
+
+from .command_classes import Command_TextObject, Command
+
+##  The list of commands applied.
+_commandUndoList = []  # type; [Command]
+
+## Save the given command on the undo list then execute the command on
+#  the given text object.
+#
+#  @param text
+#         The @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject"
+#         on which to apply the command.
+#  @param command
+#         The Command object to apply to the text object.
+def Command_Save_And_Execute(text : Command_TextObject, command : Command):
+    _commandUndoList.append(command)
+    command.Execute()
 
 
-##  Example of using the @ref command_pattern "Command" design pattern.
+## An operation to search and replace text in a
+#  @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject".
+#
+#  @param source
+#         The Command_TextObject to affect.
+#  @param searchPattern
+#         What to look for in the
+#         @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject".
+#  @param replaceText
+#         What to replace the searchPattern with.
+def Command_Operation_Replace(source : Command_TextObject, searchPattern : str, replaceText : str) -> None:
+    newText = source.Text().replace(searchPattern, replaceText)
+    source.SetText(newText)
+
+
+## An operation to reverse the characters in the given
+#  @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject".
+#
+#  @param source
+#         The @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject"
+#         to affect.
+def Command_Operation_Reverse(source : Command_TextObject):
+    text_in_list = [c for c in source.Text()]
+    text_in_list.reverse()
+    output = "".join(text_in_list)
+    source.SetText(output)
+
+
+## Perform an undo on the given
+#  @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject",
+#  using the commands in the "global" undo list.  If the undo list is empty, nothing happens.
+# 
+#  @param text
+#         The @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject"
+#         to affect.
+def Command_Undo(text : Command_TextObject) -> None:
+    if _commandUndoList:
+        # Reset the text to the starting point.
+        text.Reset()
+
+        # Get rid of the last command applied and remember it.
+        lastCommand = _commandUndoList.pop(len(_commandUndoList) - 1)
+
+        # Now apply all remaining commands to the text in order
+        # (oldest to newest).
+        for command in _commandUndoList:
+            command.Execute()
+
+        # Show off what we (un)did.
+        print("    undoing command {0:<31}==> \"{1}\"".format(lastCommand.ToString(), text.ToString()))
+
+
+## Helper function to create a @ref DesignPatternExamples_python.command.command_classes.Command "Command"
+#  object that replaces text in the given
+#  @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject",
+#  adds the command to the undo list and then applies the command
+#  to the @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject".
+#  Finally, it shows off what was done.
+#
+#  @param text
+#         The @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject"
+#         to affect.
+#  @param searchPattern
+#         What to look for in the @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject".
+#  @param replaceText
+#         What to replace the `searchPattern` with.
+def Command_ApplyReplaceCommand(text : Command_TextObject, searchPattern : str, replaceText : str) -> None:
+    command = Command(text, "Replace", Command_Operation_Replace, searchPattern, replaceText)
+    Command_Save_And_Execute(text, command)
+    print("    command {0:<31}==> \"{1}\"".format(command.ToString(), text.ToString()))
+
+
+## Helper function to create a @ref DesignPatternExamples_python.command.command_classes.Command "Command"
+#  object that reverses the order of the characters in the given
+#  @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject",
+#  adds the command to the undo list and then applies the command to the
+#  @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject".
+#  Finally, it shows what was done.
+# 
+#  @param text
+#         The @ref DesignPatternExamples_python.command.command_classes.Command_TextObject "Command_TextObject"
+#         to affect.
+def Command_ApplyReverseCommand(text : Command_TextObject) -> None:
+    command = Command(text, "Reverse", Command_Operation_Reverse)
+    Command_Save_And_Execute(text, command)
+    print("    command {0:<31}==> \"{1}\"".format(command.ToString(), text.ToString()))
+
+
+##  Example of using the @ref command_pattern.
 #  
 #  The Command pattern is used to encapsulate an operation or command
 #  associated with an object so that the command can be applied to
 #  the object at a later time.
 #  
-#  In this exercise, an undo list is implemented using Commands that
-#  associate commands defined in this file with a text object.  The
-#  commands are applied to the text object in succession then
-#  effectively undone.
+#  In this exercise, an undo list is implemented using instances of the
+#  @ref DesignPatternExamples_python.command.command_classes.Command "Command"
+#  class that associate commands defined in this file with a text object.  The
+#  commands are applied to the text object in succession then effectively undone.
 
 # ! [Using Command in Python]
 def Command_Exercise():
     print()
     print("Command Exercise")
+
+    # The base text object to work from.
+    text = Command_TextObject("This is a line of text on which to experiment.")
+
+    print("  Starting text: \"{0}\"".format(text.ToString()))
+
+    # Apply four operations to the text.
+    Command_ApplyReplaceCommand(text, "text", "painting")
+    Command_ApplyReplaceCommand(text, "on", "off")
+    Command_ApplyReverseCommand(text)
+    Command_ApplyReplaceCommand(text, "i", "!")
+
+    print("  Now perform undo until back to original")
+
+    # Now undo the four operations.
+    Command_Undo(text)
+    Command_Undo(text)
+    Command_Undo(text)
+    Command_Undo(text)
+
+    print("  Final text   : \"{0}\"".format(text.ToString()))
 
     print("  Done.")
 # ! [Using Command in Python]
