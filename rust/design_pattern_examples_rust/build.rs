6bb71ef48ee5459bff25eefd28b875dec2d24504
Stephen P. Lepisto <stephen@stephenlepisto.com>
2023-07-16 02:01:08 +0000

    Update build.rs build event to use cargo metadata to get the target directory instead of relying on an environment variable.  This requires adding a build dependency on the serde_json crate so the build.rs script can use that crate.

--- rust/design_pattern_examples_rust/build.rs
+++ rust/design_pattern_examples_rust/build.rs
@@ -6,16 +6,16 @@
 use std::env;
 use std::fs;
 use std::path::{Path, PathBuf};
+use std::process::Command;
+use serde_json;
 
 fn main() {
-    let profile = env::var("PROFILE").unwrap_or("debug".to_string());
-    let debug_config: String;
-    if profile == "debug" {
-        debug_config = "Debug".to_string();
-    }
-    else {
-        debug_config = "Release".to_string();
-    }
+    // Note: the DEBUG environment variable is not in the cargo metadata so we
+    // go to the environment for the value.
+    let debug_config = match env::var("DEBUG").unwrap().as_str() {
+        "true" => "debug",
+        _ => "release",
+    };
     let library_path = Path::join(&env::current_dir().unwrap(), "..").join("..").join("lib").join("x64").join(format!("{debug_config}/"));
     println!("cargo:rustc-link-lib=Adapter_BackEnd");
     println!("cargo:rustc-link-search={}", library_path.display());
@@ -24,6 +24,7 @@ fn main() {
     let input_path = library_path;
     let output_path = _get_output_path(&debug_config);
 
+    println!("cargo:warning=output_path = {}", output_path.display());
     // Make sure the output directory exists
     output_path.try_exists().unwrap();
 
@@ -33,18 +34,47 @@ fn main() {
         let dest = output_path.join(file);
         fs::copy(src, dest).unwrap();
     }
-    //let mut output = String::from("Environment variables in build environment\n");
-    //for e in env::vars() {
-    //    let entry = format!("  {} = {}\n", e.0, e.1);
-    //    output.push_str(&entry);
-    //}
-    //fs::write("build_env.log", output).unwrap();
 }
 
 
+/// Retrieve the path to where all output files are being written.  Need to get
+/// this from the metadata provided by cargo.
+///
+/// # Parameters
+/// - build_type
+///
+///   The build configuration type (typically "debug" or "release") that becomes
+///   a sub-directory under the target_directory where the files go.  This was
+///   determined empirically because this information is not in the cargo
+///   metadata.
+///
+/// # Returns
+/// Returns a PathBuf object containing the output path to use.  PathBuf is
+/// usually required for file-oriented functions.
 fn _get_output_path(build_type: &str) -> PathBuf {
-    //<root or manifest path>/target/<profile>/
-    let manifest_dir_string = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let path = Path::new(&manifest_dir_string).join("target").join(build_type);
-    return PathBuf::from(path);
+    let metadata = _read_cargo_metadata();
+
+    // This converts the JSON-formatted string into a map of strings to values.
+    let json_map : serde_json::Map<String, serde_json::Value> = serde_json::from_str(&metadata).unwrap();
+
+    // get() returns a double quote-delimited value for some reason so be sure
+    // to strip the double quotes.  Note that trim_matches() returns a string
+    // slice, but I also needed to call replace() and that returns a string so
+    // that's all good.
+    let target_dir = match json_map.get("target_directory") {
+        Some(v) => v.to_string().trim_matches('"').replace("\\\\", "\\"),
+        None => String::new(),
+    };
+    PathBuf::from(target_dir).join(build_type)
+}
+
+/// Read all the cargo metadata into a string and return it.  Basically, run
+/// the cargo metadata command return the output as a string.
+///
+/// # Returns
+/// Returns a string containing the cargo metadata, presumably formatted in
+/// JSON.  It's up to the caller to parse the data correctly.
+fn _read_cargo_metadata() -> String {
+    let output = Command::new("cargo").arg("metadata").arg("--format-version").arg("1").output().expect("Failed to run cargo metadata");
+    String::from_utf8_lossy(&output.stdout).to_string()
 }
