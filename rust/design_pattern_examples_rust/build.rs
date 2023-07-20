20f5cf4c4832c2c63c666d338279a0a332afef23
Stephen P. Lepisto <stephen@stephenlepisto.com>
2023-07-20 02:37:15 +0000

    CMake is now used to build DesignPatternExamples, with the following changes.
    Remove all Visual Studio project and solution files from version control.
    
    Edit Doxyfile to send all output to the build directory.
    
    Edit build.rs to only print out the linker commands for the Adapter_BackEnd dll instead of also trying to copy that file (now handled in the CMakeLists.txt file for rust).

--- rust/design_pattern_examples_rust/build.rs
+++ rust/design_pattern_examples_rust/build.rs
@@ -1,13 +1,11 @@
 //! design_pattern_examples_rust build script
 //!
-//! This build script always copies the Adapter_BackEnd library files to the
-//! target directory.
+//! This build script sets up the link switches for linking to the
+//! Adapter_BackEnd dynamic library, given a path to that library via the
+//! environment variable ADAPTER_BINARY_DIR.
 
 use std::env;
-use std::fs;
 use std::path::{Path, PathBuf};
-use std::process::Command;
-use serde_json;
 
 fn main() {
     // Note: the DEBUG environment variable is not in the cargo metadata so we
@@ -16,67 +14,18 @@ fn main() {
         "true" => "debug",
         _ => "release",
     };
-    let library_path = Path::join(&env::current_dir().unwrap(), "..").join("..").join("lib").join("x64").join(format!("{debug_config}/"));
+
+    // We expect CMake to provide the ADAPTER_BINARY_DIR variable.  However,
+    // tools such as rust analyzer in Visual Studio Code can't provide that
+    // variable and instead just stops working because the environment variable
+    // can't be found (that is, don't just call unwrap() when trying to get the
+    // variable).  If the variable cannot be found, generate a path that it
+    // might have been.
+    let library_path = match env::var("ADAPTER_BINARY_DIR") {
+        Ok(value) => PathBuf::from(value),
+        Err(_) => Path::join(&env::current_dir().unwrap(), "..").join("..").join("build").join("Adapter_BackEnd").join(format!("{debug_config}/")),
+    };
+
     println!("cargo:rustc-link-lib=Adapter_BackEnd");
     println!("cargo:rustc-link-search={}", library_path.display());
-
-    let files_to_copy = vec!["Adapter_BackEnd.dll", "Adapter_BackEnd.pdb"];
-    let input_path = library_path;
-    let output_path = _get_output_path(&debug_config);
-
-    println!("cargo:warning=output_path = {}", output_path.display());
-    // Make sure the output directory exists
-    output_path.try_exists().unwrap();
-
-    println!("cargo:warning=Copying Adapter_BackEnd files to {}", output_path.display());
-    for file in files_to_copy {
-        let src = input_path.join(file);
-        let dest = output_path.join(file);
-        if src.exists() {
-            fs::copy(src, dest).unwrap();
-        }
-    }
-}
-
-
-/// Retrieve the path to where all output files are being written.  Need to get
-/// this from the metadata provided by cargo.
-///
-/// # Parameters
-/// - build_type
-///
-///   The build configuration type (typically "debug" or "release") that becomes
-///   a sub-directory under the target_directory where the files go.  This was
-///   determined empirically because this information is not in the cargo
-///   metadata.
-///
-/// # Returns
-/// Returns a PathBuf object containing the output path to use.  PathBuf is
-/// usually required for file-oriented functions.
-fn _get_output_path(build_type: &str) -> PathBuf {
-    let metadata = _read_cargo_metadata();
-
-    // This converts the JSON-formatted string into a map of strings to values.
-    let json_map : serde_json::Map<String, serde_json::Value> = serde_json::from_str(&metadata).unwrap();
-
-    // get() returns a double quote-delimited value for some reason so be sure
-    // to strip the double quotes.  Note that trim_matches() returns a string
-    // slice, but I also needed to call replace() and that returns a string so
-    // that's all good.
-    let target_dir = match json_map.get("target_directory") {
-        Some(v) => v.to_string().trim_matches('"').replace("\\\\", "\\"),
-        None => String::new(),
-    };
-    PathBuf::from(target_dir).join(build_type)
-}
-
-/// Read all the cargo metadata into a string and return it.  Basically, run
-/// the cargo metadata command return the output as a string.
-///
-/// # Returns
-/// Returns a string containing the cargo metadata, presumably formatted in
-/// JSON.  It's up to the caller to parse the data correctly.
-fn _read_cargo_metadata() -> String {
-    let output = Command::new("cargo").arg("metadata").arg("--format-version").arg("1").output().expect("Failed to run cargo metadata");
-    String::from_utf8_lossy(&output.stdout).to_string()
 }
