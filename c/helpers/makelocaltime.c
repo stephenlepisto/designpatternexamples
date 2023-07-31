987cf47850f199e9857ce702cdad1b1a2962eb3c
Stephen P. Lepisto <stephenp@otakuworld.com>
2023-07-31 14:36:05 -0700

    Remove __STDC_WANT_LIB_EXT1__ macro.

--- c/helpers/makelocaltime.c
+++ c/helpers/makelocaltime.c
@@ -3,8 +3,6 @@
 /// Implementation of the makelocaltime() function
 /// to convert a time_t to a struct tm containing the local time.
 
-// Posix way of asking for bounds-checked versions of library functions.
-#define __STDC_WANT_LIB_EXT1__ 1
 #include <time.h>
 
 #include "makelocaltime.h"
