92eb3a7a3236de3f82c289517f336a2143670985
Stephen P. Lepisto <stephenp@otakuworld.com>
2023-07-31 14:38:05 -0700

    Rewrite to always use localtime() instead of localtime_s().

--- cplusplus/helpers/makelocaltime.cpp
+++ cplusplus/helpers/makelocaltime.cpp
@@ -3,8 +3,6 @@
 /// Implementation of the @ref Helpers::makelocaltime "makelocaltime()" function
 /// to convert a time_t to a struct tm containing the local time.
 
-// Posix way of asking for bounds-checked versions of library functions.
-#define __STDC_WANT_LIB_EXT1__ 1
 #include <time.h>
 #include <chrono>
 
@@ -21,18 +19,9 @@ namespace Helpers
         struct tm* returnstruct = nullptr;
         if (time != nullptr && timestruct != nullptr)
         {
-#ifdef _MSC_VER
-            // Note reversal of the parameters from the other forms.
-            errno_t err = localtime_s(timestruct, time);
-            if (err == 0)
-            {
-                returnstruct = timestruct;
-            }
-#elif defined(__STDC_LIB_EXT1__)
-            returnstruct = localtime_s(time, timestruct);
-#else
-            returnstruct = localtime_r(time, timestruct)
-#endif
+            returnstruct = localtime(time);
+            *timestruct = *returnstruct;
+            returnstruct = timestruct;
         }
         return returnstruct;
     }
