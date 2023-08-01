edca389a7ccf72aa9f8ed717c63c8f6a1452f723
Stephen P. Lepisto <stephen@stephenlepisto.com>
2023-08-01 15:17:21 -0700

    For Linux, implement stricmp().

--- cplusplus/helpers/stricmp.cpp
+++ cplusplus/helpers/stricmp.cpp
@@ -2,12 +2,12 @@
 /// @brief
 /// Implementation of the @ref Helpers::stricmp "stricmp" function, case-insensitive
 /// string comparison for narrow character strings.
-/// 
+///
 /// Function to compare two strings in a case-insensitive way.  Handles
 /// const char* and std::string as arguments.
 
 #include <string>
-#include <string.h>
+#include <cstring>
 
 #include "stricmp.h"
 
@@ -15,12 +15,50 @@ namespace Helpers
 {
     int stricmp(const char* first, const char* second)
     {
+#ifdef _MSC_VER
         return _stricmp(first, second);
+#else
+        int result = -1;
+        if (first != nullptr && second != nullptr)
+        {
+            result = 0;
+            while (*first != '\0' && *second != '\0')
+            {
+                char first_c = toupper(*first);
+                char second_c = toupper(*second);
+                if (first_c < second_c)
+                {
+                    result = -1;
+                    break;
+                }
+                if (first_c > second_c)
+                {
+                    result = 1;
+                    break;
+                }
+                first++;
+                second++;
+            }
+            if (result == 0 && *first != *second)
+            {
+                // shorter string is lexically before longer string.
+                if (*first == '\0')
+                {
+                    result = -1;
+                }
+                else
+                {
+                    result = 1;
+                }
+            }
+        }
+        return result;
+#endif
     }
 
     int stricmp(const std::string& first, const std::string& second)
     {
-        return _stricmp(first.c_str(), second.c_str());
+        return stricmp(first.c_str(), second.c_str());
     }
 
 } // end namespace
